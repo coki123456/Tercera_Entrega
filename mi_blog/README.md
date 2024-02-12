03a591690148cb4577eae078cacb327e216abbbc
coki123456 <esteban_coki@hotmail.com>
2024-02-12 17:13:29 -0300

    5to Commit, proyecto casi terminado

---
+++ mi_blog/README.md
@@ -0,0 +1,1 @@
+Projecto creado por: Esteban Alvarez Farhat
--- mi_blog/pag_inicio/forms.py
+++ mi_blog/pag_inicio/forms.py
@@ -1,7 +1,8 @@
 from django import forms
+from datetime import datetime
 
 class FormularioCreacionPost(forms.Form):
     titulo = forms.CharField(max_length=20)
     autor = forms.CharField(max_length=30)
-    texto = forms.CharField()
-    fecha_post = forms.DateTimeField()
+    texto = forms.CharField(widget=forms.Textarea, max_length=500)
+    fecha_post = forms.DateTimeField(initial=datetime.now)
